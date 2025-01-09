bb4f83802089663083ae6ba8505dbfac3fc2e505
Piyush <piyush.tailor@rosh.ai>
2025-01-09 16:48:20 +0530

    test for the differrent system

--- py_data_collection/py_data_collection/data_collection.py
+++ py_data_collection/py_data_collection/data_collection.py
@@ -1,11 +1,12 @@
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Imu, LaserScan, Image
+from sensor_msgs.msg import Imu, LaserScan, Image,PointCloud2
 from tf2_ros import TransformListener, Buffer
 import h5py
 import numpy as np
-from sensor_msgs_py import point_cloud2  # For image handling (if needed)
+import time
+import json
 
 class HDF5_Write(Node):
     def __init__(self):
@@ -48,16 +49,7 @@ class HDF5_Write(Node):
         # Create or open an HDF5 file
         self.h5_file = h5py.File('sensor_datas.h5', 'w')
 
-        # Create groups for storing data
-        self.odom_group = self.h5_file.create_group('odom_data')
-        self.odom_header_group = self.odom_group.create_group('header')
-        self.odom_header_stamp_group = self.odom_header_group.create_group('stamp')
-        self.odom_pose_group = self.odom_group.create_group('pose')
-        self.odom_pose_pose_group = self.odom_pose_group.create_group('pose')
 
-        self.imu_group = self.h5_file.create_group('imu_data')
-        self.imu_header_group = self.imu_group.create_group('header')
-        self.imu_header_stamp_group = self.imu_header_group.create_group('stamp')
 
 
         self.lidar_group = self.h5_file.create_group('lidar_data')
@@ -71,124 +63,156 @@ class HDF5_Write(Node):
         self.camera_group = self.h5_file.create_group('camera_data')  # New group for storing Camera data
         self.camera_header_group = self.camera_group.create_group('header')
         self.camera_header_stamp_group = self.camera_header_group.create_group('stamp')
+
+
+        self.odom_data=self.h5_file.create_dataset('odom_data', shape=(1,),maxshape=(None, ),chunks=(1, ),  data='S512',compression="gzip")
+        self.imu_data=self.h5_file.create_dataset('imu_data', shape=(1,),maxshape=(None, ),chunks=(1, ),  data='S512',compression="gzip")
+        self.laserscan_data=self.h5_file.create_dataset('laserscan_data', shape=(1,),maxshape=(None, ),chunks=(1, ),  data='S512',compression="gzip")
+          
         
         # Create a timer to call tf_callback every 100ms (0.1 seconds)
         self.create_timer(0.1, self.tf_callback)  # Call tf_callback every 100ms
+    def odom_JSON(self,msg:Odometry):
+        return  {
+                'header': {
+                    'frame_id': msg.header.frame_id,
+                    'stamp': {
+                        'sec': msg.header.stamp.sec,
+                        'nanosec': msg.header.stamp.nanosec
+                    }
+                },
+                'pose': {
+                    'position': {
+                        'x': msg.pose.pose.position.x,
+                        'y': msg.pose.pose.position.y,
+                        'z': msg.pose.pose.position.z
+                    },
+                    'orientation': {
+                        'x': msg.pose.pose.orientation.x,
+                        'y': msg.pose.pose.orientation.y,
+                        'z': msg.pose.pose.orientation.z,
+                        'w': msg.pose.pose.orientation.w
+                    }
+                },
+                'twist': {
+                    'linear': {
+                        'x': msg.twist.twist.linear.x,
+                        'y': msg.twist.twist.linear.y,
+                        'z': msg.twist.twist.linear.z
+                    },
+                    'angular': {
+                        'x': msg.twist.twist.angular.x,
+                        'y': msg.twist.twist.angular.y,
+                        'z': msg.twist.twist.angular.z
+                    }
+                }
+            }
 
+    def imu_JSON(self,msg:Imu):
+        return {
+                    'header': {
+                        'frame_id': msg.header.frame_id,
+                        'stamp': {
+                            'sec': msg.header.stamp.sec,
+                            'nanosec': msg.header.stamp.nanosec
+                        }
+                    },
+                    'orientation': {
+                        'x': msg.orientation.x,
+                        'y': msg.orientation.y,
+                        'z': msg.orientation.z,
+                        'w': msg.orientation.w
+                    },
+                    'angular_velocity': {
+                        'x': msg.angular_velocity.x,
+                        'y': msg.angular_velocity.y,
+                        'z': msg.angular_velocity.z
+                    },
+                    'linear_acceleration': {
+                        'x': msg.linear_acceleration.x,
+                        'y': msg.linear_acceleration.y,
+                        'z': msg.linear_acceleration.z
+                    }
+                }
+
+    def laserscan_JSON(self,msg:LaserScan):
+        return {
+        'header': {
+            'frame_id': msg.header.frame_id,
+            'stamp': {
+                'sec': msg.header.stamp.sec,
+                'nanosec': msg.header.stamp.nanosec
+            }
+        },
+        'angle_min': msg.angle_min,
+        'angle_max': msg.angle_max,
+        'angle_increment': msg.angle_increment,
+        'time_increment': msg.time_increment,
+        'scan_time': msg.scan_time,
+        'range_min': msg.range_min,
+        'range_max': msg.range_max,
+        # 'ranges': msg.ranges,  # List of range values (distance measurements)
+        # 'intensities': msg.intensities  # List of intensity values (optional)
+        }
+
+    def PointCloud2_JSON(self,msg:PointCloud2):
+        return  {
+                'header': {
+                    'frame_id': msg.header.frame_id,
+                    'stamp': {
+                        'sec': msg.header.stamp.sec,
+                        'nanosec': msg.header.stamp.nanosec
+                    }
+                },
+                'height': msg.height,
+                'width': msg.width,
+                'fields': [{'name': field.name,
+                            'offset': field.offset,
+                            'datatype': field.datatype,
+                            'count': field.count} for field in msg.fields],
+                'is_bigendian': msg.is_bigendian,
+                'point_step': msg.point_step,
+                'row_step': msg.row_step,
+                'is_dense': msg.is_dense,
+                'data': []  # This will store the actual point data
+            }
+
+    
     def odom_callback(self, msg: Odometry):
-        position = msg.pose.pose.position
-        orientation = msg.pose.pose.orientation
 
-        if 'position' not in self.odom_pose_pose_group:
-            self.odom_header_group.create_dataset('frame_id',shape=(1,1),maxshape=(None, 1),chunks=(1, 1), data=msg.header.frame_id)      
-            self.odom_header_stamp_group.create_dataset('sec',shape=(1,),maxshape=(None, ),chunks=(1, ), data=np.array([msg.header.stamp.sec], dtype=np.float64))
-            self.odom_header_stamp_group.create_dataset('nanosec', shape=(1,),maxshape=(None, ),chunks=(1, ),data=np.array([msg.header.stamp.nanosec / 1e9], dtype=np.float64))
-            self.odom_pose_pose_group.create_dataset('position',shape=(1,3),maxshape=(None, 3),chunks=(1, 3),data=[position.x, position.y, position.z])
-            self.odom_pose_pose_group.create_dataset('orientation', shape=(1,4),maxshape=(None, 4),chunks=(1, 4),  data=[orientation.x, orientation.y, orientation.z, orientation.w])
-          
-        else:
-            self.odom_header_group['frame_id'].resize(self.odom_header_group['frame_id'].shape[0] + 1, axis=0)
-            self.odom_header_group['frame_id'][-1] = msg.header.frame_id
-            self.odom_header_stamp_group['sec'].resize(self.odom_header_stamp_group['sec'].shape[0] + 1, axis=0)
-            self.odom_header_stamp_group['sec'][-1] = msg.header.stamp.sec
-            self.odom_header_stamp_group['nanosec'].resize(self.odom_header_stamp_group['nanosec'].shape[0] + 1, axis=0)
-            self.odom_header_stamp_group['nanosec'][-1] = msg.header.stamp.nanosec
-            self.odom_pose_pose_group['position'].resize(self.odom_pose_pose_group['position'].shape[0] + 1, axis=0)
-            self.odom_pose_pose_group['position'][-1] = np.array([position.x, position.y, position.z])
-            self.odom_pose_pose_group['orientation'].resize(self.odom_pose_pose_group['orientation'].shape[0] + 1, axis=0)
-            self.odom_pose_pose_group['orientation'][-1] = np.array([orientation.x, orientation.y, orientation.z,orientation.w], dtype=np.float64)
+        odometry_data=self.odom_JSON(msg)   
+        # Serialize the dictionary to a JSON string
+        odometry_json = json.dumps(odometry_data)
+  
+        self.odom_data.resize(self.odom_data.shape[0] + 1, axis=0)
+        self.odom_data[-1] = np.string_(odometry_json)
 
 
         self.get_logger().info(f"Odometry data received at {msg.header.stamp.sec:.2f}")
 
 
     def imu_callback(self, msg: Imu):
-        # Extract data from IMU message
-        acceleration = [msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z]
-        angular_velocity = [msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z]
-        orientation = [msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]
-        timestamp = msg.header.stamp.sec + msg.header.stamp.nanosec / 1e9
+        imu_data=self.imu_JSON(msg)   
+        # Serialize the dictionary to a JSON string
+        imu_json = json.dumps(imu_data)
+  
+        self.imu_data.resize(self.imu_data.shape[0] + 1, axis=0)
+        self.imu_data[-1] = np.string_(imu_json)
 
-        acceleration_data = np.array(acceleration, dtype=np.float64)
-        angular_velocity_data = np.array(angular_velocity, dtype=np.float64)
-        orientation_data = np.array(orientation, dtype=np.float64)
 
-        if 'linear_acceleration' not in self.imu_group:
-            self.imu_group.create_dataset(
-                'linear_acceleration',
-                shape=(1, 3),
-                data=acceleration_data,
-                maxshape=(None, 3),
-                chunks=(1, 3),
-                compression="gzip"
-            )
-            self.imu_group.create_dataset(
-                'angular_velocity',
-                shape=(1, 3),
-                data=angular_velocity_data,
-                maxshape=(None, 3),
-                chunks=(1, 3),
-                compression="gzip"
-            )
-            self.imu_group.create_dataset(
-                'orientation',
-                shape=(1, 4),
-                data=orientation_data,
-                maxshape=(None, 4),
-                chunks=(1, 4),
-                compression="gzip"
-            )
-            self.imu_header_group.create_dataset('frame_id',shape=(1,1),maxshape=(None, 1),chunks=(1, 1), data=msg.header.frame_id)      
-            self.imu_header_stamp_group.create_dataset('sec',shape=(1,),maxshape=(None, ),chunks=(1, ), data=np.array([msg.header.stamp.sec], dtype=np.float64))
-            self.imu_header_stamp_group.create_dataset('nanosec', shape=(1,),maxshape=(None, ),chunks=(1, ),data=np.array([msg.header.stamp.nanosec / 1e9], dtype=np.float64))
-        else:
-            self.imu_header_group['frame_id'].resize(self.imu_header_group['frame_id'].shape[0] + 1, axis=0)
-            self.imu_header_group['frame_id'][-1] = msg.header.frame_id
-            self.imu_header_stamp_group['sec'].resize(self.imu_header_stamp_group['sec'].shape[0] + 1, axis=0)
-            self.imu_header_stamp_group['sec'][-1] = msg.header.stamp.sec
-            self.imu_header_stamp_group['nanosec'].resize(self.imu_header_stamp_group['nanosec'].shape[0] + 1, axis=0)
-            self.imu_header_stamp_group['nanosec'][-1] = msg.header.stamp.nanosec
-            self.imu_group['linear_acceleration'].resize(self.imu_group['linear_acceleration'].shape[0] + 1, axis=0)
-            self.imu_group['linear_acceleration'][-1] = acceleration_data
-            self.imu_group['angular_velocity'].resize(self.imu_group['angular_velocity'].shape[0] + 1, axis=0)
-            self.imu_group['angular_velocity'][-1] = angular_velocity_data
-            self.imu_group['orientation'].resize(self.imu_group['orientation'].shape[0] + 1, axis=0)
-            self.imu_group['orientation'][-1] = orientation_data
-
-        self.get_logger().info(f"IMU data received at {timestamp:.2f}")
+        self.get_logger().info(f"Odometry data received at {msg.header.stamp.sec:.2f}")
 
     def lidar_callback(self, msg: LaserScan):
-        # Extract ranges and timestamp from the LaserScan message
-        ranges = msg.ranges
- 
+        laserscan_data=self.laserscan_JSON(msg)   
+        # Serialize the dictionary to a JSON string
+        laserscan_json = json.dumps(laserscan_data)
+  
+        self.laserscan_data.resize(self.laserscan_data.shape[0] + 1, axis=0)
+        self.laserscan_data[-1] = np.string_(laserscan_json)
 
-        ranges_data = np.array(ranges, dtype=np.float64)
 
-        if 'ranges' not in self.lidar_group:
-            self.lidar_group.create_dataset(
-                'ranges',
-                shape=(1, len(ranges)),
-                data=ranges_data,
-                maxshape=(None, len(ranges)),
-                chunks=(1, len(ranges)),
-                compression="gzip"
-            )
-            self.lidar_header_group.create_dataset('frame_id',shape=(1,1),maxshape=(None, 1),chunks=(1, 1), data=msg.header.frame_id)      
-            self.lidar_header_stamp_group.create_dataset('sec',shape=(1,),maxshape=(None, ),chunks=(1, ), data=np.array([msg.header.stamp.sec], dtype=np.float64))
-            self.lidar_header_stamp_group.create_dataset('nanosec', shape=(1,),maxshape=(None, ),chunks=(1, ),data=np.array([msg.header.stamp.nanosec / 1e9], dtype=np.float64))
-          
-        else:
-            self.lidar_header_group['frame_id'].resize(self.lidar_header_group['frame_id'].shape[0] + 1, axis=0)
-            self.lidar_header_group['frame_id'][-1] = msg.header.frame_id
-            self.lidar_header_stamp_group['sec'].resize(self.lidar_header_stamp_group['sec'].shape[0] + 1, axis=0)
-            self.lidar_header_stamp_group['sec'][-1] = msg.header.stamp.sec
-            self.lidar_header_stamp_group['nanosec'].resize(self.lidar_header_stamp_group['nanosec'].shape[0] + 1, axis=0)
-            self.lidar_header_stamp_group['nanosec'][-1] = msg.header.stamp.nanosec
-           
-            self.lidar_group['ranges'].resize(self.lidar_group['ranges'].shape[0] + 1, axis=0)
-            self.lidar_group['ranges'][-1] = ranges_data
+        self.get_logger().info(f"Odometry data received at {msg.header.stamp.sec:.2f}")
 
-        self.get_logger().info(f"LiDAR scan data received at {msg.header.stamp.sec:.2f}")
 
     def camera_callback(self, msg: Image):
         # Extract image data and timestamp from the camera image message
@@ -204,9 +228,9 @@ class HDF5_Write(Node):
                 chunks=(1, len(image_data)),
                 compression="gzip"
             )
-            self.camera_header_group.create_dataset('frame_id',shape=(1,1),maxshape=(None, 1),chunks=(1, 1), data=msg.header.frame_id)      
-            self.camera_header_stamp_group.create_dataset('sec',shape=(1,),maxshape=(None, ),chunks=(1, ), data=np.array([msg.header.stamp.sec], dtype=np.float64))
-            self.camera_header_stamp_group.create_dataset('nanosec', shape=(1,),maxshape=(None, ),chunks=(1, ),data=np.array([msg.header.stamp.nanosec / 1e9], dtype=np.float64))
+            self.camera_header_group.create_dataset('frame_id',shape=(1,1),maxshape=(None, 1),chunks=(1, 1), data=msg.header.frame_id,compression="gzip")      
+            self.camera_header_stamp_group.create_dataset('sec',shape=(1,),maxshape=(None, ),chunks=(1, ), data=np.array([msg.header.stamp.sec], dtype=np.float64),compression="gzip")
+            self.camera_header_stamp_group.create_dataset('nanosec', shape=(1,),maxshape=(None, ),chunks=(1, ),data=np.array([msg.header.stamp.nanosec / 1e9], dtype=np.float64),compression="gzip")
  
         else:
             self.camera_header_group['frame_id'].resize(self.camera_header_group['frame_id'].shape[0] + 1, axis=0)
@@ -241,9 +265,9 @@ class HDF5_Write(Node):
                     chunks=(1, 7),
                     compression="gzip"
                 )
-                self.tf_header_group.create_dataset('frame_id',shape=(1,1),maxshape=(None, 1),chunks=(1, 1), data=transform.header.frame_id)      
-                self.tf_header_stamp_group.create_dataset('sec',shape=(1,),maxshape=(None, ),chunks=(1, ), data=np.array([transform.header.stamp.sec], dtype=np.float64))
-                self.tf_header_stamp_group.create_dataset('nanosec', shape=(1,),maxshape=(None, ),chunks=(1, ),data=np.array([transform.header.stamp.nanosec / 1e9], dtype=np.float64))
+                self.tf_header_group.create_dataset('frame_id',shape=(1,1),maxshape=(None, 1),chunks=(1, 1), data=transform.header.frame_id,compression="gzip")      
+                self.tf_header_stamp_group.create_dataset('sec',shape=(1,),maxshape=(None, ),chunks=(1, ), data=np.array([transform.header.stamp.sec], dtype=np.float64),compression="gzip")
+                self.tf_header_stamp_group.create_dataset('nanosec', shape=(1,),maxshape=(None, ),chunks=(1, ),data=np.array([transform.header.stamp.nanosec / 1e9], dtype=np.float64),compression="gzip")
     
             else:
                 self.tf_header_group['frame_id'].resize(self.tf_header_group['frame_id'].shape[0] + 1, axis=0)
--- py_data_collection/py_data_collection/data_publishing.py
+++ py_data_collection/py_data_collection/data_publishing.py
@@ -19,26 +19,32 @@ class HDF5_Read(Node):
 
         # Open the HDF5 file
         self.h5_file = h5py.File('sensor_datas.h5', 'r')
-        self.index =0
+        self.odom_index =0
+        self.imu_index =0
+        self.lidar_index =0
+        self.camera_data_index =0
         # Create a timer to call the publish_data every 1 second
         
-        self.create_timer(0.1, self.publish_data)  # Timer callback every 1 second
+        self.create_timer(0.1, self.publish_odom)  # Timer callback every 1 second
+        self.create_timer(0.1, self.publish_imu)  # Timer callback every 1 second
+        self.create_timer(0.1, self.publish_lidar)  # Timer callback every 1 second
+        self.create_timer(0.1, self.publish_camera_data)  # Timer callback every 1 second
 
-    def publish_data(self):
+    def publish_odom(self):
         # Publish Odometry Data
         if 'odom_data' in self.h5_file:
             odom_group = self.h5_file['odom_data']
-            if(self.index<len(odom_group['header/stamp/sec'])):
+            if(self.odom_index<len(odom_group['header/stamp/sec'])):
                 header = Header()
-                header.stamp.sec = int(odom_group['header/stamp/sec'][self.index])
-                header.stamp.nanosec = int(odom_group['header/stamp/nanosec'][self.index]*1e9)
+                header.stamp.sec = int(odom_group['header/stamp/sec'][self.odom_index])
+                header.stamp.nanosec = int(odom_group['header/stamp/nanosec'][self.odom_index]*1e9)
 
                 odom_msg = Odometry()
                 odom_msg.header = header
-                odom_msg.header.frame_id = str(odom_group['header/frame_id'][self.index])
+                odom_msg.header.frame_id = str(odom_group['header/frame_id'][self.odom_index])
 
-                position = odom_group['pose/pose/position'][self.index]
-                orientation = odom_group['pose/pose/orientation'][self.index]
+                position = odom_group['pose/pose/position'][self.odom_index]
+                orientation = odom_group['pose/pose/orientation'][self.odom_index]
                 odom_msg.pose.pose.position.x = position[0]
                 odom_msg.pose.pose.position.y = position[1]
                 odom_msg.pose.pose.position.z = position[2]
@@ -48,79 +54,89 @@ class HDF5_Read(Node):
                 odom_msg.pose.pose.orientation.w = orientation[3]
 
                 self.odom_publisher.publish(odom_msg)
+                self.odom_index=self.odom_index+1
+                self.get_logger().info("Published odom_index to topics.")
             else:
-                print("The odometry data was out of range")
+                print("The odom data was out of range with index: ",self.odom_index)
 
         # Publish IMU Data
+    def publish_imu(self):
         if 'imu_data' in self.h5_file:
             imu_group = self.h5_file['imu_data']
-            if(self.index<len(imu_group['header/stamp/sec'])):
+            if(self.imu_index<len(imu_group['header/stamp/sec'])):
                 header = Header()
-                header.stamp.sec = int(imu_group['header/stamp/sec'][self.index])
-                header.stamp.nanosec = int(imu_group['header/stamp/nanosec'][self.index]*1e9)
+                header.stamp.sec = int(imu_group['header/stamp/sec'][self.imu_index])
+                header.stamp.nanosec = int(imu_group['header/stamp/nanosec'][self.imu_index]*1e9)
 
                 imu_msg = Imu()
                 imu_msg.header = header
-                imu_msg.header.frame_id = str(imu_group['header/frame_id'][self.index])
+                imu_msg.header.frame_id = str(imu_group['header/frame_id'][self.imu_index])
 
                 # Acceleration
-                imu_msg.linear_acceleration.x = imu_group['linear_acceleration'][self.index][0]
-                imu_msg.linear_acceleration.y = imu_group['linear_acceleration'][self.index][1]
-                imu_msg.linear_acceleration.z = imu_group['linear_acceleration'][self.index][2]
+                imu_msg.linear_acceleration.x = imu_group['linear_acceleration'][self.imu_index][0]
+                imu_msg.linear_acceleration.y = imu_group['linear_acceleration'][self.imu_index][1]
+                imu_msg.linear_acceleration.z = imu_group['linear_acceleration'][self.imu_index][2]
 
                 # Angular Velocity
-                imu_msg.angular_velocity.x = imu_group['angular_velocity'][self.index][0]
-                imu_msg.angular_velocity.y = imu_group['angular_velocity'][self.index][1]
-                imu_msg.angular_velocity.z = imu_group['angular_velocity'][self.index][2]
+                imu_msg.angular_velocity.x = imu_group['angular_velocity'][self.imu_index][0]
+                imu_msg.angular_velocity.y = imu_group['angular_velocity'][self.imu_index][1]
+                imu_msg.angular_velocity.z = imu_group['angular_velocity'][self.imu_index][2]
 
                 # Orientation
-                imu_msg.orientation.x = imu_group['orientation'][self.index][0]
-                imu_msg.orientation.y = imu_group['orientation'][self.index][1]
-                imu_msg.orientation.z = imu_group['orientation'][self.index][2]
-                imu_msg.orientation.w = imu_group['orientation'][self.index][3]
+                imu_msg.orientation.x = imu_group['orientation'][self.imu_index][0]
+                imu_msg.orientation.y = imu_group['orientation'][self.imu_index][1]
+                imu_msg.orientation.z = imu_group['orientation'][self.imu_index][2]
+                imu_msg.orientation.w = imu_group['orientation'][self.imu_index][3]
 
                 self.imu_publisher.publish(imu_msg)
+                self.imu_index=self.imu_index+1
+                self.get_logger().info("Published imu_index to topics.")
             else:
-                print("The imu_group data was out of range")
+                print("The imu data was out of range with index: ",self.imu_index)
         # Publish LiDAR Data
+    def publish_lidar(self):
         if 'lidar_data' in self.h5_file:
             lidar_group = self.h5_file['lidar_data']
-            if(self.index<len(lidar_group['header/stamp/sec'])):
+            if(self.lidar_index<len(lidar_group['header/stamp/sec'])):
 
                 header = Header()
-                header.stamp.sec = int(lidar_group['header/stamp/sec'][self.index])
-                header.stamp.nanosec = int(lidar_group['header/stamp/nanosec'][self.index]*1e9)
+                header.stamp.sec = int(lidar_group['header/stamp/sec'][self.lidar_index])
+                header.stamp.nanosec = int(lidar_group['header/stamp/nanosec'][self.lidar_index]*1e9)
 
                 lidar_msg = LaserScan()
                 lidar_msg.header = header
-                lidar_msg.header.frame_id = str(lidar_group['header/frame_id'][self.index])
-                lidar_msg.ranges = lidar_group['ranges'][self.index]
+                lidar_msg.header.frame_id = str(lidar_group['header/frame_id'][self.lidar_index])
+                lidar_msg.ranges = lidar_group['ranges'][self.lidar_index]
 
                 self.lidar_publisher.publish(lidar_msg)
+                self.lidar_index=self.lidar_index+1
+                self.get_logger().info("Published lidar_index to topics.")
             else:
-                print("The lidar_group data was out of range")
+                print("The lidar data was out of range with index: ",self.lidar_index)
 
             
 
         # Publish Camera Image Data
+    def publish_camera_data(self):
         if 'camera_data' in self.h5_file:
             camera_group = self.h5_file['camera_data']
-            if(self.index<len(camera_group['header/stamp/sec'])):
+            if(self.camera_data_index<len(camera_group['header/stamp/sec'])):
                 header = Header()
-                header.stamp.sec = int(camera_group['header/stamp/sec'][self.index])
-                header.stamp.nanosec = int(camera_group['header/stamp/nanosec'][self.index]*1e9)
+                header.stamp.sec = int(camera_group['header/stamp/sec'][self.camera_data_index])
+                header.stamp.nanosec = int(camera_group['header/stamp/nanosec'][self.camera_data_index]*1e9)
 
                 camera_msg = Image()
                 camera_msg.header = header
-                camera_msg.header.frame_id = str(camera_group['header/frame_id'][self.index])
-                camera_msg.data = camera_group['image'][self.index].tolist()  # Convert numpy array to list for Image message
+                camera_msg.header.frame_id = str(camera_group['header/frame_id'][self.camera_data_index])
+                camera_msg.data = camera_group['image'][self.camera_data_index].tolist()  # Convert numpy array to list for Image message
 
 
                 self.camera_publisher.publish(camera_msg)
+                self.camera_data_index=self.camera_data_index+1
+                self.get_logger().info("Published camera_data_index to topics.")
             else:
-                print("The camera_group data was out of range")
-        self.index=self.index+1
-        self.get_logger().info("Published data to topics.")
+                print("The camera_group data was out of range with index: ",self.camera_data_index)
+
 
     def __del__(self):
         # Close the HDF5 file when done
